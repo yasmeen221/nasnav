147ec58ec7760747e23c42f97a6798184b656db9
yasmeen221 <yasmienmostafa38@gmail.com>
2024-04-23 23:05:20 +0200

    fix error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,23 @@ import { Route, Router, Routes } from "react-router-dom";
 
 import Layout from "../Layout";
 import { lazy } from "react";
+import Toolbar from "../components/Toolbar/Toolbar";
+import Cards from "../components/Cards/Cards";
+import Footer from "../components/Footer/Footer";
+import ImageCard from "./../components/ImageCard/ImageCard";
 
 const Home = () => {
-  const ImageCard = lazy(() => import("../components/ImageCard/ImageCard"));
+  // const ImageCard = lazy(() => import("../components/ImageCard/ImageCard"));
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Layout />} />
         <Route index element={<ImageCard />} />
-      </Routes>
-      {/* <Toolbar />
+      </Routes> */}
+      <Toolbar />
       <ImageCard />
       <Cards />
-      <Footer /> */}
+      <Footer />
     </>
   );
 };
